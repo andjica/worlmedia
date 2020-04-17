12ad2e519e110445f878322232b6456551425df4
andjica <andjaaa95@gmail.com>
2020-04-17 18:40:26 +0200

    update route

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('/admin-home', 'AdminController@index')->name('admin-home');
 //user
 Route::post('/upgrade-user', 'UserController@store')->name('upgrade-user');
 Route::post('/upgrade-info', 'UserController@storeinfo')->name('upgrade-info');
+Route::post('/edit-user', 'UserController@update')->name('edit-user');
 
 Route::post('/active-pro', 'PaymentController@activepro')->name('active-pro');
 Route::get('/status-pro', 'PaymentController@statuspro')->name('status-pro'); 
