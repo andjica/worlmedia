544be77b47cc21bcb109b24e8bda1e1f18ce8490
andjica <andjaaa95@gmail.com>
2020-04-18 20:36:59 +0200

    adding some object

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Purchase;
+use App\User;
 
 class AdminController extends Controller
 {
     public function index()
     {
         $countpurchases = Purchase::count();
-        return view('pages.admin-home');
+        $totalearn = Purchase::sum('price');
+        $countuser = User::count();
+
+        $users = User::paginate(6);
+        
+
+        return view('pages.admin-home', compact('countpurchases', 'totalearn', 'countuser'));
     }
 }
--- resources/views/components/admin-manager.blade.php
+++ resources/views/components/admin-manager.blade.php
@@ -1,14 +1,56 @@
 <div class="col-lg-3">
         <div class="card">
             <div class="card-header">
-                Featured
+                Status of purchases
             </div>
             <div class="card-body text-center">
                 <h5 class="card-title">Special purchases</h5>
                 <div class="counter col_fourth end">
                 <i class="fa fa-shopping-cart fa-3x text-orange"></i>
-                <h2 class="timer count-title count-number" data-to="150" data-speed="1500"></h2>
-                <p class="count-text ">Total Purchase</p>
+                @if($countpurchases == null)
+                    0
+                @else
+                <h2 class="timer count-title count-number" data-to="{{$countpurchases}}" data-speed="1500"></h2>
+                @endif
+                <p class="count-text ">Total number of Purchase</p>
+             </div>
+            </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+        <div class="card">
+            <div class="card-header">
+            Status of your earnings
+            </div>
+            <div class="card-body text-center">
+                <h5 class="card-title">Special purchases</h5>
+                <div class="counter col_fourth end">
+                <i class="fa fa-cc-paypal fa-3x text-orange"></i>
+                @if($totalearn == null)
+                    0
+                @else
+                <h2 class="timer count-title count-number" data-to="{{$totalearn}}" data-speed="1500"></h2>
+                @endif
+                <p class="count-text "> Value of total payed accounts</p>
+             </div>
+            </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+        <div class="card">
+            <div class="card-header">
+                Registered users
+            </div>
+            <div class="card-body text-center">
+                <h5 class="card-title">Special purchases</h5>
+                <div class="counter col_fourth end">
+                <i class="fa fa-user fa-3x text-orange"></i>
+                @if($countuser == null)
+                    0
+                @else
+                <h2 class="timer count-title count-number" data-to="{{$countuser}}" data-speed="1500"></h2>
+                @endif
+                <p class="count-text "> Number of your users</p>
              </div>
             </div>
             </div>
@@ -18,34 +60,17 @@
             <div class="card-header">
                 Featured
             </div>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-            </div>
-        </div>
-        <div class="col-lg-3">
-        <div class="card">
-            <div class="card-header">
-                Featured
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-            </div>
-        </div>
-        <div class="col-lg-3">
-        <div class="card">
-            <div class="card-header">
-                Featured
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card-body text-center">
+                <h5 class="card-title">Special purchases</h5>
+                <div class="counter col_fourth end">
+                <i class="fa fa-trophy fa-3x text-orange"></i>
+                @if($totalearn == null)
+                    0
+                @else
+                <h2 class="timer count-title count-number" data-to="{{$totalearn}}" data-speed="1500"></h2>
+                @endif
+                <p class="count-text ">Best ranked users</p>
+             </div>
             </div>
             </div>
         </div>
