dfa6ecd56515bbd6c198657176ef1822e3216be1
andjica <andjaaa95@gmail.com>
2020-04-18 20:13:19 +0200

    new components

---
+++ resources/views/components/admin-manager.blade.php
@@ -0,0 +1,51 @@
+<div class="col-lg-3">
+        <div class="card">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body text-center">
+                <h5 class="card-title">Special purchases</h5>
+                <div class="counter col_fourth end">
+                <i class="fa fa-shopping-cart fa-3x text-orange"></i>
+                <h2 class="timer count-title count-number" data-to="150" data-speed="1500"></h2>
+                <p class="count-text ">Total Purchase</p>
+             </div>
+            </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+        <div class="card">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+        <div class="card">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+        <div class="card">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+        </div>
