bd17ca995745aa04d67ae04aa35886d34e699003
andjica <andjaaa95@gmail.com>
2020-04-18 20:12:59 +0200

    adding model

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Purchase;
 
 class AdminController extends Controller
 {
     public function index()
     {
+        $countpurchases = Purchase::count();
         return view('pages.admin-home');
     }
 }
