f7dbb6ba8358ec76579dd1e1c65f87cec7937913
andjica <andjaaa95@gmail.com>
2020-04-19 00:23:38 +0200

    adding middleware

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,27 @@ Auth::routes();
 Route::get('/editprofile', 'HomeController@editprofile')->name('editprofile');
 Route::get('/home', 'HomeController@index')->name('home');
 
-//admin
-Route::get('/admin-home', 'AdminController@index')->name('admin-home');
 //user
 Route::post('/upgrade-user', 'UserController@store')->name('upgrade-user');
 Route::post('/upgrade-info', 'UserController@storeinfo')->name('upgrade-info');
 Route::post('/edit-user', 'UserController@update')->name('edit-user');
 
+Route::group(['middleware' => ['checkaccount']], function () 
+{
+
+    //admin
+    Route::get('/admin-home', 'AdminController@index')->name('admin-home');
+
+    //city
+    Route::get('/admin-cities', 'CityController@index')->name('admin-cities');
+    Route::get('/edit-city/{id}', 'CityController@edit')->name('edit-city');
+    Route::get('/delete-city/{id}', 'CityController@destroy')->name('delete-city');
+
+});
+
+
+
+//payment
 Route::post('/active-pro', 'PaymentController@activepro')->name('active-pro');
 Route::get('/status-pro', 'PaymentController@statuspro')->name('status-pro'); 
 
