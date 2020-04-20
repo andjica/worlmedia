ff001ee1b9e7d44f52f6a188d07071965341c019
andjica <andjaaa95@gmail.com>
2020-04-20 15:52:31 +0200

    edit blade

--- resources/views/components/admin/users.blade.php
+++ resources/views/components/admin/users.blade.php
@@ -2,14 +2,28 @@
 <div class="col-lg-4">
         <div class="card">
             <div class="card-header">
-                #id: {{$user->id}}
+                <b class="text-info">#id: {{$user->id}}</b>, <small>Created at: {{$user->created_at->format('d-m-Y')}}</small>
             </div>
             <div class="card-body">
+            @if($user->name == null && $user->url == null)
+                <h5 class="card-title">Name: null</h5>
+                <div class="rounded-circle mt-2 image-background"  style="">
+                </div>
+                <small></small>
+            @else
                 <h5 class="card-title">Name: {{$user->name}}</h5>
                 <div class="rounded-circle mt-2 image-background"  style="background-image: url({{asset('/image-users/'.$user->url)}});">
                 </div>
-                <p class="count-text ">Email: {{$user->email}}</p>
-                <small>{{$user->city->name}}, {{$user->city->country->name_country}}</small>
+            @endif
+                <small>Email: {{$user->email}}<br>
+                @if($user->city_id == null && $user->category_id == null)
+
+                @else
+                Category:&nbsp;{{$user->category->name}}<br>
+                Place: &nbsp;{{$user->city->name}}, {{$user->city->country->name_country}}</small>
+               @endif
+               <br>
+               <a href="" class="btn btn-sm btn-info"><small>View more</small></a>
              </div>
             </div>
             </div>
