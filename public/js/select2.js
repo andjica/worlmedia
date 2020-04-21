ff9fff0b00981649ba85b0fa3c15c5bdd5e717e2
andjica <andjaaa95@gmail.com>
2020-04-21 17:06:40 +0200

    new js

---
+++ public/js/select2.js
@@ -0,0 +1,13 @@
+     // select2 
+     $('.js-example-basic-single').select2({
+        placeholder: "Category",
+        allowClear: true,
+        
+      });
+      // select2
+      $('.js-example-basic-single2').select2({
+        placeholder: "City",
+        allowClear: true
+      });
+    
+      
