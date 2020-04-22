6eab58b8c90453e95ff396e31c6ff602aee18ca0
andjica <andjaaa95@gmail.com>
2020-04-22 16:25:40 +0200

    make rate

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Review;
+use App\User;
 
 class ReviewController extends Controller
 {
-    //
+    public function rate()
+    {
+        $rate = request()->rate;
+        $userid = request()->userid;
+        
+        $review = new Review();
+        
+        $review->user_id = $userid;
+        $review->user_respond = 1;
+        $review->comment = "andjica daje komentare";
+        $review->rate = $rate;
+
+        try{
+            $review->save();
+            return redirect()->back()->with('success', 'Thank for giving rate');
+        }
+        catch(\Throwable $e)
+        {
+            return redirect()->back()->with('error', 'Oops, something went wrong, try latter');
+        }
+    }
 }
