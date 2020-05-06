62e25dd26dd3fa051fc6c276fab143026b8b1163
andjica <andjaaa95@gmail.com>
2020-05-06 19:14:23 +0200

    some blades changes

--- resources/views/components/editprofile.blade.php
+++ resources/views/components/editprofile.blade.php
@@ -32,7 +32,7 @@
                     <a class="list-group-item list-group-item-action" data-toggle="list" href="#password" role="tab">
                       Change password
                     </a>
-                    <a class="list-group-item list-group-item-action" data-toggle="list" href="#contact" role="tab">
+                    <a class="list-group-item list-group-item-action" data-toggle="list" href="#skill" role="tab">
                       Manage skills
                     </a>
                     <a class="list-group-item list-group-item-action" data-toggle="list" href="#contact" role="tab">
@@ -238,7 +238,34 @@
                         </div>
                     </div>
                 </div>
+                <div class="tab-pane " id="skill" role="tabpanel">
+                <div class="card">
+                        <div class="card-header">
+                            <div class="card-actions float-right">
+                                <div class="dropdown show">
+                                    <a href="#" data-toggle="dropdown" data-display="static">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal align-middle">
+                                            <circle cx="12" cy="12" r="1"></circle>
+                                            <circle cx="19" cy="12" r="1"></circle>
+                                            <circle cx="5" cy="12" r="1"></circle>
+                                        </svg>
+                                    </a>
 
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <h5 class="card-title mb-0">Your information</h5>
+                        </div>
+                        <div class="card-body">
+                            @include('components.skills')
+
+                        </div>
+                    </div>
+                </div>
                 <div class="tab-pane " id="contact" role="tabpanel">
                     <div class="card">
                         <div class="card-body">
---
+++ resources/views/components/skills.blade.php
@@ -0,0 +1,147 @@
+<form action="{{route('add-skills')}}" method="post" id="">
+                        @csrf
+                                <div class="form-group">
+                               
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-4">
+                                        <label for="Mobile">Skill one</label>
+                                        <input type="text" class="form-control" id="sk1" name="sk1" value="">
+                                        <p id="er-mobile" class="text-danger"></p> 
+                                        <p id="su-mobile" class="text-info"></p> 
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="Link">Percent</label>
+                                        <input type="text" class="form-control" id="pr1" name="pr1" value="">
+                                         <p id="er-url" class="text-danger"></p> 
+                                         <p id="su-url" class="text-info"></p> 
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-4">
+                                        <label for="Mobile">Skill two</label>
+                                        <input type="text" class="form-control" id="sk2" name="sk2" value="">
+                                        <p id="er-mobile" class="text-danger"></p> 
+                                        <p id="su-mobile" class="text-info"></p> 
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="Link">Percent</label>
+                                        <input type="text" class="form-control" id="pr2" name="pr2" value="">
+                                         <p id="er-url" class="text-danger"></p> 
+                                         <p id="su-url" class="text-info"></p> 
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-4">
+                                        <label for="Mobile">Skill Three</label>
+                                        <input type="text" class="form-control" id="sk3" name="sk3" value="">
+                                        <p id="er-mobile" class="text-danger"></p> 
+                                        <p id="su-mobile" class="text-info"></p> 
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="Link">Percent</label>
+                                        <input type="text" class="form-control" id="pr3" name="pr3" value="">
+                                         <p id="er-url" class="text-danger"></p> 
+                                         <p id="su-url" class="text-info"></p> 
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-4">
+                                        <label for="Mobile">Skill Four</label>
+                                        <input type="text" class="form-control" id="sk4" name="sk4" value="">
+                                        <p id="er-mobile" class="text-danger"></p> 
+                                        <p id="su-mobile" class="text-info"></p> 
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="Link">Percent</label>
+                                        <input type="text" class="form-control" id="pr4" name="pr4" value="">
+                                         <p id="er-url" class="text-danger"></p> 
+                                         <p id="su-url" class="text-info"></p> 
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-4">
+                                        <label for="Mobile">Skill five</label>
+                                        <input type="text" class="form-control" id="sk5" name="sk5" value="">
+                                        <p id="er-mobile" class="text-danger"></p> 
+                                        <p id="su-mobile" class="text-info"></p> 
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="Link">Percent</label>
+                                        <input type="text" class="form-control" id="pr5" name="pr5" value="">
+                                         <p id="er-url" class="text-danger"></p> 
+                                         <p id="su-url" class="text-info"></p> 
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-4">
+                                        <label for="Mobile">Skill Six</label>
+                                        <input type="text" class="form-control" id="sk6" name="sk6" value="">
+                                        <p id="er-mobile" class="text-danger"></p> 
+                                        <p id="su-mobile" class="text-info"></p> 
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="Link">Percent</label>
+                                        <input type="text" class="form-control" id="pr6" name="pr6" value="">
+                                         <p id="er-url" class="text-danger"></p> 
+                                         <p id="su-url" class="text-info"></p> 
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-4">
+                                        <label for="Mobile">Skill Seven</label>
+                                        <input type="text" class="form-control" id="sk7" name="sk7" value="">
+                                        <p id="er-mobile" class="text-danger"></p> 
+                                        <p id="su-mobile" class="text-info"></p> 
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="Link">Percent</label>
+                                        <input type="text" class="form-control" id="pr7" name="pr7" value="">
+                                         <p id="er-url" class="text-danger"></p> 
+                                         <p id="su-url" class="text-info"></p> 
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-4">
+                                        <label for="Mobile">Skill eight</label>
+                                        <input type="text" class="form-control" id="sk8" name="sk8" value="">
+                                        <p id="er-mobile" class="text-danger"></p> 
+                                        <p id="su-mobile" class="text-info"></p> 
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="Link">Percent</label>
+                                        <input type="text" class="form-control" id="pr8" name="pr8" value="">
+                                         <p id="er-url" class="text-danger"></p> 
+                                         <p id="su-url" class="text-info"></p> 
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-4">
+                                        <label for="Mobile">Skill Nine</label>
+                                        <input type="text" class="form-control" id="sk9" name="sk9" value="">
+                                        <p id="er-mobile" class="text-danger"></p> 
+                                        <p id="su-mobile" class="text-info"></p> 
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="Link">Percent</label>
+                                        <input type="text" class="form-control" id="pr9" name="pr9" value="">
+                                         <p id="er-url" class="text-danger"></p> 
+                                         <p id="su-url" class="text-info"></p> 
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                <div class="form-group col-md-4">
+                                        <label for="Mobile">Skill Ten</label>
+                                        <input type="text" class="form-control" id="sk10" name="sk10" value="">
+                                        <p id="er-mobile" class="text-danger"></p> 
+                                        <p id="su-mobile" class="text-info"></p> 
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="Link">Percent</label>
+                                        <input type="text" class="form-control" id="pr10" name="pr10" value="">
+                                         <p id="er-url" class="text-danger"></p> 
+                                         <p id="su-url" class="text-info"></p> 
+                                    </div>
+                                </div>
+                                <input type="submit" class="btn btn-primary" value="Save changes">
+                            </form>
