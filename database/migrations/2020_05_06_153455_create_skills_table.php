1d4a02bf48d24ce49f1ca177f21bba992a8fab9f
andjica <andjaaa95@gmail.com>
2020-05-06 18:42:21 +0200

    migrations update

--- database/migrations/2020_05_06_153455_create_skills_table.php
+++ database/migrations/2020_05_06_153455_create_skills_table.php
@@ -15,9 +15,33 @@ class CreateSkillsTable extends Migration
     {
         Schema::create('skills', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('skill_one');
+            $table->integer('percent_one');
+            $table->string('skill_two');
+            $table->integer('percent_two');
+            $table->string('skill_three');
+            $table->integer('percent_three');
+            $table->string('skill_four');
+            $table->integer('percent_four');
+            $table->string('skill_five');
+            $table->integer('percent_five');
+            $table->string('skill_six');
+            $table->integer('percent_six');
+            $table->string('skill_seven');
+            $table->integer('percent_seven');
+            $table->string('skill_eight');
+            $table->integer('percent_eight');
+            $table->string('skill_nine');
+            $table->integer('percent_nine');
+            $table->string('skill_ten');
+            $table->integer('percent_ten');
+            $table->integer('user_id')->unsigned()->index();
             $table->timestamps();
 
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
