5a74cf834735237cb685fba94eeea158dd256309
andjica <andjaaa95@gmail.com>
2020-05-11 19:26:03 +0200

    updated model user

--- app/Follower.php
+++ app/Follower.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Follower extends Model
 {
     protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
