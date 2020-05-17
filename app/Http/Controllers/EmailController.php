8240735c32271c9dc34f068bdfa932886969fa26
andjica <andjaaa95@gmail.com>
2020-05-17 18:14:27 +0200

    new method

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Mail\ContactEmail;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactEmail;
+use App\Mail\MatchEmail;
+use App\Review;
 
 class EmailController extends Controller
 {
@@ -29,5 +31,30 @@ class EmailController extends Controller
         {
             return abort(500);
         }
+    }
+
+    public function match()
+    {
+        $data = ([
+            'email' => request()->userauth,
+            'subject' => 'match',
+            'message' => request()->message
+        ]);
+
+        $emailto = request()->useremail;
+    
+        
+            Mail::cc($emailto, 'developersforanymarket@gmail.com')->send(new MatchEmail($data));
+            
+            $review = new Review();
+            $userIdAuth = auth()->user()->id;
+            $review->user_id =  $userIdAuth;
+            $review->is_matched_id = request()->userid;
+            $review->matched_status = 0;
+            $review->save();
+
+            return back()->with('success', 'You matched successfully');
+        
+      
     }
 }
